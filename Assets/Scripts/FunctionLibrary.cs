7b7ac8dc57c159a2e7851e80a47be2bbe8db7502
onuratasaritas <onur.saritas@bites.com.tr>
2021-01-29 00:13:49 +0300

    Add 3rd dimension

---
+++ Assets/Scripts/FunctionLibrary.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using static UnityEngine.Mathf;
+
+public static class FunctionLibrary
+{
+
+    public delegate Vector3 Function(float x, float z, float t);
+
+    static Function[] functions = { Wave, MultiWave, Ripple };
+
+    public enum FunctionName { Wave, MultiWave, Ripple};
+
+    public static Function GetFunction(FunctionName name)
+    {
+        return functions[(int)name];
+    }
+	public static Vector3 Wave(float u, float v, float t)
+	{
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (u + v + t));
+		p.z = v;
+		return p;
+	}
+
+	public static Vector3 MultiWave(float u, float v, float t)
+	{
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (u + 0.5f * t));
+		p.y += 0.5f * Sin(2f * PI * (v + t));
+		p.y += Sin(PI * (u + v + 0.25f * t));
+		p.y *= 1f / 2.5f;
+		p.z = v;
+		return p;
+	}
+
+	public static Vector3 Ripple(float u, float v, float t)
+	{
+		float d = Sqrt(u * u + v * v);
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (4f * d - t));
+		p.y /= 1f + 10f * d;
+		p.z = v;
+		return p;
+	}
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+
 public class Graph : MonoBehaviour
 {
     [SerializeField]
@@ -8,35 +9,42 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+
     Transform[] points;
-    
 
-    private void Awake()
-    {
-        Vector3 position = Vector3.zero;
-        float step = 2f / resolution;
-        var scale = Vector3.one * step;
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++)
-        {
-            Transform point = Instantiate(pointPrefab);
-            points[i] = point;
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
-            point.localScale = scale;
-            point.SetParent(transform, false);
-        }
-    }
 
-    private void Update()
-    {
-        float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
-        {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-            point.localPosition = position;
-        }
-    }
+	void Awake()
+	{
+		float step = 2f / resolution;
+		var scale = Vector3.one * step;
+		points = new Transform[resolution * resolution];
+		for (int i = 0; i < points.Length; i++)
+		{
+			Transform point = Instantiate(pointPrefab);
+			point.localScale = scale;
+			point.SetParent(transform, false);
+			points[i] = point;
+		}
+	}
+
+	void Update()
+	{
+		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+		float time = Time.time;
+		float step = 2f / resolution;
+		float v = 0.5f * step - 1f;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+		{
+			if (x == resolution)
+			{
+				x = 0;
+				z += 1;
+				v = (z + 0.5f) * step - 1f;
+			}
+			float u = (x + 0.5f) * step - 1f;
+			points[i].localPosition = f(u, v, time);
+		}
+	}
 }
